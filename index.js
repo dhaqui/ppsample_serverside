8340d8acab8951980bb06c7d55f436c9d68d8e01
dhaqui <49834807+dhaqui@users.noreply.github.com>
2024-09-09 18:47:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors');  // corsミドルウェアの読み込み
 const app = express();
 
 app.use(bodyParser.json());
 
-const cors = require('cors');
+// CORSを有効にする（1つだけcorsを使います）
 app.use(cors({
   origin: 'https://dhaqui.github.io',  // GitHub Pagesのオリジンを指定
+  methods: ['GET', 'POST'],  // 許可するHTTPメソッド
+  allowedHeaders: ['Content-Type', 'Authorization']  // 許可するヘッダー
 }));
-
-// CORSを有効にするミドルウェア
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');  // 任意のオリジンを許可
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 const PAYPAL_CLIENT_ID = 'EAMWkTh00y7VV_OF0rjxXvriOLYOCOINlwQLfwuif4HjSxSfOFcvI3TV5363vM1svOPqyX00HtlQIepu'; // あなたのPayPal Client ID
 const PAYPAL_CLIENT_SECRET = 'EAMWkTh00y7VV_OF0rjxXvriOLYOCOINlwQLfwuif4HjSxSfOFcvI3TV5363vM1svOPqyX00HtlQIepu'; // あなたのPayPal Client Secret
@@ -26,7 +20,6 @@ const PAYPAL_API_URL = 'https://api-m.sandbox.paypal.com'; // 本番環境は 'h
 // PayPalのアクセストークンを生成
 const generateAccessToken = async () => {
   const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`).toString('base64');
-
   try {
     const response = await axios.post(`${PAYPAL_API_URL}/v1/oauth2/token`, 'grant_type=client_credentials', {
       headers: {
@@ -43,13 +36,12 @@ const generateAccessToken = async () => {
 // PayPalオーダーを作成
 app.post('/create-order', async (req, res) => {
   const accessToken = await generateAccessToken();
-
   const order = {
     intent: 'CAPTURE',
     purchase_units: [{
       amount: {
         currency_code: 'JPY',
-        value: '3600'  // 合計金額
+        value: '3600'
       }
     }]
   };
@@ -89,3 +81,12 @@ app.post('/capture-order', async (req, res) => {
 
 // Vercelはポートを自動で設定します
 module.exports = app;
+
+
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
