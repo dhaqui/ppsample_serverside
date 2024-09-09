69c69dd11e368acb4f7968535a30d89d1ef11120
dhaqui <49834807+dhaqui@users.noreply.github.com>
2024-09-09 18:42:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require('cors');  
 const app = express();
 
 app.use(bodyParser.json());
