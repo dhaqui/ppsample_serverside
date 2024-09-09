1ccae4c01684032bc2056812ac1aa8eceabab74f
dhaqui <49834807+dhaqui@users.noreply.github.com>
2024-09-10 01:47:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
   // 許可するヘッダー
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
 
   // プリフライトリクエスト（OPTIONSリクエスト）に対応
   if (req.method === 'OPTIONS') {
