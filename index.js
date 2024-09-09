39965ac4498da3b3524870678283bdbe4650fbe7
dhaqui <49834807+dhaqui@users.noreply.github.com>
2024-09-10 00:40:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express();
 
 // CORS設定
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://dhaqui.github.io'); // 必要なオリジンを許可
+  // 特定のオリジン（例: 'https://dhaqui.github.io'）のみ許可
+  res.setHeader('Access-Control-Allow-Origin', 'https://dhaqui.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Cookieを必要とする場合
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Credentialsの送信を許可
   if (req.method === 'OPTIONS') {
-    res.sendStatus(204); // プリフライトリクエストに対して、OKレスポンスを返す
+    res.sendStatus(204); // プリフライトリクエストに対して、204 No Contentを返す
   } else {
     next();
   }
